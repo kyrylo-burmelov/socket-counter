7f25467783d8dbb223faeee57df96c408b5c28b1
Oleksii30 <polyanovskyyol@gmail.com>
2023-02-09 14:32:09 +0200

    upd socket.io to v4 & counterWithRooms methods

--- socket/counterWithRooms.js
+++ socket/counterWithRooms.js
@@ -1,24 +1,26 @@
 const rooms = ["ROOM_1", "ROOM_2", "ROOM_3"];
 const counterMap = new Map(rooms.map(roomId => [roomId, 0]));
 
-const getCurrentRoomId = socket => Object.keys(socket.rooms).find(roomId => counterMap.has(roomId));
+const getCurrentRoomId = socket => rooms.find(roomId => socket.rooms.has(roomId));
 
 export default io => {
   io.on("connection", socket => {
     socket.emit("UPDATE_ROOMS", rooms);
 
     socket.on("JOIN_ROOM", roomId => {
-      const prevRoomId = getCurrentRoomId(socket);
+      const prevRoomId = getCurrentRoomId(socket)
+
       if (roomId === prevRoomId) {
         return;
       }
       if (prevRoomId) {
         socket.leave(prevRoomId);
       }
-      socket.join(roomId, () => {
-        const counterValue = counterMap.get(roomId);
-        io.to(socket.id).emit("JOIN_ROOM_DONE", { counterValue, roomId });
-      });
+
+      socket.join(roomId);
+      const counterValue = counterMap.get(roomId);
+      io.to(socket.id).emit("JOIN_ROOM_DONE", { counterValue, roomId });
+
     });
 
     socket.on("INCREASE_COUNTER", () => {
